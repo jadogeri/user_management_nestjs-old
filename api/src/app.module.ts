3389871fff2f1354e489f82e6cdb7d583a628629
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2026-01-30 13:28:55 -0600

    Added resource Profile

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
+import { UserModule } from './resources/user/user.module';
+import { AuthModule } from './resources/auth/auth.module';
+import { ProfileModule } from './resources/profile/profile.module';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [UserModule, AuthModule, ProfileModule],
   controllers: [AppController],
   providers: [AppService],
 })
