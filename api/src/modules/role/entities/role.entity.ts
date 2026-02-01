de2688d1171bba2073b1e801912de0f906a38b93
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-02-01 13:55:20 -0600

    Updated Role entity

--- api/src/modules/role/entities/role.entity.ts
+++ api/src/modules/role/entities/role.entity.ts
@@ -1,1 +1,19 @@
-export class Role {}
+import { Permission } from 'src/modules/permission/entities/permission.entity';
+import { UserRole } from 'src/shared/enums/user-role.enum';
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+
+@Entity('roles')
+export class Role {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: 'enum',
+    enum: UserRole,
+    unique: true, // Ensures each role exists only once in the table
+  })
+  name: UserRole;
+
+  @OneToMany(() => Permission, (permission: Permission) => permission.role)
+  permissions: Permission[];
+}
